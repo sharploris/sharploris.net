ad83cd87cb8e31a8461a13f7c9f08a2d8c6f3780
Daniel Williams <danielw093@gmail.com>
2020-03-05 17:50:24 +0000

    Added material ui packages and got icons displaying correctly

--- components/Profile/index.tsx
+++ components/Profile/index.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styles from './index.module.scss';
+import { Twitter, LinkedIn, GitHub, EmailOutlined } from '@material-ui/icons';
 
 interface IProfileProps {
   personName: string;
@@ -9,28 +10,52 @@ interface IProfileProps {
   contactEmail?: string;
 }
 
+enum SocialType {
+  Twitter = "Twitter",
+  LinkedIn = "LinkedIn",
+  GitHub = "GitHub",
+  Email = "Email"
+}
+
 export default class Profile extends Component<IProfileProps, {}> {
     render() {
       return (
         <div>
           <h3>{this.props.personName}</h3>
-          <span className={styles.socialLinks}>
-            {this.props.twitterUrl && this.renderLink("Twitter", this.props.twitterUrl, "mdi mdi-twitter")}
-            {this.props.linkedinUrl && this.renderLink("LinkedIn", this.props.linkedinUrl, "mdi mdi-linkedin")}
-            {this.props.githubUrl && this.renderLink("GitHub", this.props.githubUrl, "mdi mdi-github-circle")}
-            {this.props.contactEmail && this.renderLink("Email", `mailto:${this.props.contactEmail}`, "mdi mdi-email-outline")}
-          </span>
+          {this.props.twitterUrl && this.renderLink(SocialType.Twitter, this.props.twitterUrl)}
+          {this.props.linkedinUrl && this.renderLink(SocialType.LinkedIn, this.props.linkedinUrl)}
+          {this.props.githubUrl && <span className={styles.githubIcon}>{this.renderLink(SocialType.GitHub, this.props.githubUrl)}</span>}
+          {this.props.contactEmail && this.renderLink(SocialType.Email, `mailto:${this.props.contactEmail}`)}
         </div>
       );
     }
 
-    private renderLink(accessibilityName: string, url: string, iconName: string){
+    private renderLink(type: SocialType, url: string){
       return (
-        <a href={url} target="_blank" rel="noopener noreferrer" className={`${styles.icon} ${iconName}`}>
-          <span className="hidden-text">
-            {accessibilityName}
-          </span>
+        <a href={url} title={type.toString()} target="_blank" rel="noopener noreferrer" className={styles.icon}>
+          {this.getIcon(type)}
         </a>
       );
+    }
+
+    private getIcon(type: SocialType) {
+      switch(type) {
+        case SocialType.Twitter:
+          return <Twitter style={{fontSize: 72}}>{this.renderAccessibilityText(type)}</Twitter>;
+        case SocialType.LinkedIn:
+          return <LinkedIn style={{fontSize: 72}}>{this.renderAccessibilityText(type)}</LinkedIn>;
+        case SocialType.GitHub:
+          return <GitHub style={{fontSize: 60}}>{this.renderAccessibilityText(type)}</GitHub>;
+        case SocialType.Email:
+          return <EmailOutlined style={{fontSize: 72}}>{this.renderAccessibilityText(type)}</EmailOutlined>
+      }
+    }
+
+    private renderAccessibilityText(type: SocialType) {
+      return (
+        <span className="hidden-text">
+          {type.toString()}
+        </span>
+      )
     }
   }
