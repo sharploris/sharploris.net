0df49efecded225932daeaed9891583bbde1d91f
Daniel Williams <danielw093@gmail.com>
2020-03-09 00:50:18 +0000

    Fixed minor issue

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,8 +1,9 @@
-import { withRouter, NextRouter } from 'next/router';
+import { withRouter } from 'next/router';
 import Layout from '../../components/Layout';
 import { Component } from 'react';
 import { WithRouterProps } from 'next/dist/client/with-router';
 import fetch from 'isomorphic-unfetch'
+import { NextPageContext } from 'next';
 
 interface IPostProps extends WithRouterProps {
   show: IShow;
@@ -31,7 +32,7 @@ class Post extends Component<IPostProps, {}> {
     );
   }
 
-  public static async getInitialProps(context: NextRouter) {
+  public static async getInitialProps(context: NextPageContext) {
     const { id } = context.query;
 
     const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
