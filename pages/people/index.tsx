ab68c7fbb5df378ca07b49f6015473b74ba8fe7b
Daniel Williams <danielw093@gmail.com>
2020-03-14 11:36:07 +0000

    Changed email addresses

--- pages/people/index.tsx
+++ pages/people/index.tsx
@@ -22,7 +22,7 @@ export default class PeoplePage extends Component {
                     linkedinUrl="https://www.linkedin.com/in/daniel-williams-4295b8a8"
                     githubUrl="https://github.com/DanielW093"
                     portfolioUrl="http://danielwilliams.sharploris.net/"
-                    contactEmail="contact@sharploris.net"
+                    contactEmail="daniel@sharploris.net"
                   />
                   <h4>Software Engineer and Game Developer</h4>
                 </Grid>
@@ -38,7 +38,7 @@ export default class PeoplePage extends Component {
                     twitterUrl="https://twitter.com/jessabellerina"
                     linkedinUrl="https://www.linkedin.com/in/jessmolloyga/"
                     portfolioUrl="http://jessmolloy.sharploris.net/"
-                    contactEmail="jessmolloyga@gmail.com"
+                    contactEmail="jess@sharploris.net"
                   />
                   <h4>Games Design, Art & Accessibility</h4>
                 </Grid>
@@ -53,7 +53,7 @@ export default class PeoplePage extends Component {
                     personName="Thomas Bassett"
                     githubUrl="https://github.com/snoogle13"
                     linkedinUrl="https://www.linkedin.com/in/thomas-bassett-b08304112/"
-                    contactEmail="thomasbassett94@gmail.com"
+                    contactEmail="thomas@sharploris.net"
                   />
                   <h4>Programmer on <a href="https://twitter.com/Oiseaudev" target="_blank" rel="noopener noreferrer">Oiseau</a></h4>
                   <h4>Studied Computer Science (Games Programming) at Kingston University</h4>
